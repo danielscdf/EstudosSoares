d2764e44b69449c7429bc2771792314b1bf53dae
Daniel Soares <daniel.conceicao@mirante>
2025-03-19 18:36:03 -0300

    Inclusão de arquivos java

---
+++ aprendizadoJava/DevDojo/JavaOnline01/src/Arrays1.java
@@ -0,0 +1,11 @@
+public class Arrays1 {
+    public static void main(String[] args) {
+        float[] idades = new float[5];
+        idades[0] = 20F;
+        idades[1] = 15;
+        idades[2] = 30;
+        System.out.println("idade1: "+idades[0]);
+        System.out.println("idade2: "+idades[1]);
+        System.out.println("idade3: "+idades[2]);
+    }
+}
